91e20829f6d6449d66c070080993f8f83b6bd78c
CodieAarish <mohammedaarish240@gmail.com>
2025-06-01 22:24:23 +0530

    Chapter 1

---
+++ Chapter 1/modules.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+print("Printing joke.....")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+# so thanks
+# that was my program
+# another line
+# yet another line
