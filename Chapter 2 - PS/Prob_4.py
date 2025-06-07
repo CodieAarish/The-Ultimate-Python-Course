4112412a09fd81a3fefa33d46d9d9665092f7805
CodieAarish <mohammedaarish240@gmail.com>
2025-06-07 16:14:52 +0530

    Chapter 2 - PS

---
+++ Chapter 2 - PS/Prob_4.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the value of a:- "))
+
+b = int(input("Enter the value of b:- "))
+
+print("is a is greater than b", a>b)
---
+++ Chapter 2 - PS/Prob_6.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the value of a:- "))
+
+b = a**2
+
+print(f"The square of {a} is {b}")
