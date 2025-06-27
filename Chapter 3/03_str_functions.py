4533ca9e854e0675c79335e34553543c769e91f8
CodieAarish <mohammedaarish240@gmail.com>
2025-06-27 21:02:31 +0530

    Chapter 3

---
+++ Chapter 3/03_str_functions.py
@@ -0,0 +1,6 @@
+name = "Aarish"
+
+print(len(name))
+print(name.endswith("ish"))
+print(name.startswith("ha"))
+print(name.capitalize())
---
+++ Chapter 3/04_escape_seq.py
@@ -0,0 +1,4 @@
+a = 'Aarish is a good boy\nbut not a bad \'boy\''
+
+
+print(a)
